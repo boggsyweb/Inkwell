3dd41db64a3c5b1fe8ce7bf807880c94e9182b33
bosspigg <123977427+boggsyweb@users.noreply.github.com>
2024-01-20 17:28:28 +0000

    added cookie consent

--- Inkwell/src/App.jsx
+++ Inkwell/src/App.jsx
@@ -1,11 +1,11 @@
 import * as React from 'react'
 import Header from "./components/Header";
-// import Footer from "./components/Footer";
 import Landing from './sections/Landing';
 import About from "./sections/About";
 import Services from "./sections/Services";
 import FAQ from './sections/FAQ';
 import ContactModal from './components/ContactModal';
+import CookieConsent from "react-cookie-consent"
 
 
 function App() {
@@ -19,6 +19,20 @@ function App() {
     <About />
     <Services />
     <FAQ />
+    <CookieConsent
+        enableDeclineButton
+        declineButtonText="I decline"
+        buttonText="I accept"
+        location="bottom"
+        cookieName="myAppCookieConsent"
+        style={{ background: "#1D2A6B", padding: "1em", justifyContent: "space-around" }}
+        contentStyle={{ fontSize: "14px"}}
+        buttonStyle={{ backgroundColor: "#6dff60", color: "#4e503b", fontSize: "14px", padding: ".4em", borderRadius: "5px"}}
+        declineButtonStyle={{ backgroundColor: "#FF3B3A", color: "#F5F5F5", fontSize: "14px", padding: ".4em", borderRadius: "5px"}}
+        expires={150}
+        debug={true}>
+          This website may use cookies to enhance the user experience.
+    </CookieConsent>
     </>
   )
 }
